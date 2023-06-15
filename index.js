b69ea15169f23ee48ef3444ab6dd3f59e7ec97e4
Henry Spivey <henryspivey2@gmail.com>
2023-06-14 20:16:55 -0700

    handle invalid url

--- index.js
+++ index.js
@@ -18,17 +18,31 @@ app.get('/', function (req, res) {
 
 const URL_DICT = {}
 
+const isValidUrl = urlString => {
+  var urlPattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+  return !!urlPattern.test(urlString);
+}
+
 // Your first API endpoint
 app.post('/api/shorturl/', function (req, res) {
-  const short_url_int = Object.keys(URL_DICT).length + 1 // ensure that we have at least 1 open spot
   const original_url = req.body.url
+  if (!isValidUrl(original_url)) {
+    res.json({ error: 'invalid url' })
+    return
+  }
+  const short_url_int = Object.keys(URL_DICT).length + 1 // ensure that we have at least 1 open spot
+
   URL_DICT[short_url_int] = original_url
   res.json({ original_url, short_url: short_url_int });
 });
 
 app.get('/api/shorturl/:id', function (req, res) {
   const { id } = req.params
-
   const original_url = URL_DICT[id]
   res.redirect(original_url)
 
